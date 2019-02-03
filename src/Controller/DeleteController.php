500306bc0b6308559108a3d752a7f2965096f39f
Iliya Pisany <i.pisany@yandex.ru>
2019-02-04 02:32:15 +0300

    Functionality to remove author

--- src/Controller/DeleteController.php
+++ src/Controller/DeleteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Author;
 use App\Entity\Book;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,5 +24,23 @@ class DeleteController extends AbstractController
         $db->flush();
 
         return $this->redirectToRoute('Books');
+    }
+
+
+    /**
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @Route("/authors/{id<\d+>}/delete", name="DeleteAuthor")
+     */
+    public function DeleteAuthor($id) {
+        $AuthroRepository = $this->getDoctrine()->getRepository(Author::class);
+
+        $DeletedAuthor = $AuthroRepository->find($id);
+
+        $db = $this->getDoctrine()->getManager();
+        $db->remove($DeletedAuthor);
+        $db->flush();
+
+        return $this->redirectToRoute('Authors');
     }
 }
