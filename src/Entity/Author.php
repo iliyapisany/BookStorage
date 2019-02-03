dd449e2a524fb39180e0c7e7ff6688739361d015
Iliya Pisany <i.pisany@yandex.ru>
2019-02-03 22:56:03 +0300

    Create entity and repository for authors

---
+++ src/Entity/Author.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AuthorRepository")
+ */
+class Author
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var integer
+     * @ORM\Column(type="date")
+     */
+    private $publicated_year;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=20)
+     */
+    private $ISBN;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPublicatedYear(): int
+    {
+        return $this->publicated_year;
+    }
+
+    /**
+     * @param int $publicated_year
+     */
+    public function setPublicatedYear(int $publicated_year): void
+    {
+        $this->publicated_year = $publicated_year;
+    }
+
+    /**
+     * @return string
+     */
+    public function getISBN(): string
+    {
+        return $this->ISBN;
+    }
+
+    /**
+     * @param string $ISBN
+     */
+    public function setISBN(string $ISBN): void
+    {
+        $this->ISBN = $ISBN;
+    }
+}
