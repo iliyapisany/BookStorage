8c59ae1c695c7e5ff3eae9b0586050d7d3fd7f8b
Iliya Pisany <i.pisany@yandex.ru>
2019-02-04 00:26:58 +0300

    Creating book functionality

--- src/Controller/CreateController.php
+++ src/Controller/CreateController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Form\BookType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CreateController extends AbstractController
@@ -11,9 +14,23 @@ class CreateController extends AbstractController
      * @return \Symfony\Component\HttpFoundation\Response
      * @Route("/book/create", name="CreateBook")
      */
-    public function CreateBook() {
-        return $this->render('edit/book.html.twig', [
+    public function CreateBook(Request $request) {
+        $Book = new Book();
 
+        $form = $this->createForm(BookType::class, $Book);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $db = $this->getDoctrine()->getManager();
+            $db->persist($Book);
+            $db->flush();
+
+            return $this->redirectToRoute('Books');
+        }
+
+        return $this->render('edit/base_create_form.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 
