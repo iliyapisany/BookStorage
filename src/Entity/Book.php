44e7ae47a873f21f6b51db6ed8e7d8c6c01e34bc
Iliya Pisany <i.pisany@yandex.ru>
2019-02-04 01:13:38 +0300

    Fix repository class on book entity

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="BookRepository")
+ * @ORM\Entity(repositoryClass="App\Repository\BookRepository")
  */
 class Book
 {
