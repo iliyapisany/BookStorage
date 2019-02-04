a5b5825d36cfa5a2f0a17f6890d7fdb24158993a
Iliya Pisany <i.pisany@yandex.ru>
2019-02-04 15:59:33 +0300

    Migration to save unique constraints for ISBN and name + year for book entity to db

---
+++ src/Migrations/Version20190204101137.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190204101137 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE UNIQUE INDEX ISBN_unique ON book (ISBN)');
+        $this->addSql('CREATE UNIQUE INDEX NameYearUnique ON book (name, publicated_year)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX ISBN_unique ON book');
+        $this->addSql('DROP INDEX NameYearUnique ON book');
+    }
+}
